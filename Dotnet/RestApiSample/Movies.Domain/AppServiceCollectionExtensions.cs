a13532bcd3b7803a8c0a4bb39928adf437c2fd5c
Antonio Magallon <amagallon2@outlook.com>
2025-05-02 14:49:57 -0400

    start setup domain layer, add app injection

---
+++ Dotnet/RestApiSample/Movies.Domain/AppServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Movies.Domain.Repositories;
+
+namespace Movies.Domain;
+
+public static class AppServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddSingleton<IMovieRepository, MovieRepository>();
+        return services;
+    }
+}
---
+++ Dotnet/RestApiSample/Movies.Domain/Repositories/IMovieRepository.cs
@@ -0,0 +1,16 @@
+﻿using Movies.Domain.Models;
+
+namespace Movies.Domain.Repositories;
+
+public interface IMovieRepository
+{
+   Task<bool> CreateAsync(Movie movie);
+
+   Task<Movie?> GetByIdAsync(Guid id);
+   
+   Task<IEnumerable<Movie>> GetAllAsync();
+   
+   Task<bool> UpdateAsync(Movie movie);
+   
+   Task<bool> DeleteAsync(Guid id);
+}
